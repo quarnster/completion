3cf70a75dd6402318799ca589b2041e0f355280b
Fredrik Ehnbom <fredrik@ehnbom.nu>
2013-03-16 09:51:46 +0100

    content: Basic settings structure. For #36

---
+++ content/settings.go
@@ -0,0 +1,31 @@
+package content
+
+type Settings struct {
+	data map[string]interface{}
+}
+
+func NewSettings() *Settings {
+	return &Settings{data: make(map[string]interface{})}
+}
+
+func (s *Settings) Get(key string) interface{} {
+	return s.data[key]
+}
+
+func (s *Settings) Set(key string, val interface{}) {
+	s.data[key] = val
+}
+
+func (s *Settings) Clone() *Settings {
+	s2 := NewSettings()
+	for k, v := range s.data {
+		s2.Set(k, v)
+	}
+	return s2
+}
+
+func (s *Settings) Merge(other *Settings) {
+	for k, v := range other.data {
+		s.data[k] = v
+	}
+}
---
+++ content/settings_test.go
@@ -0,0 +1,71 @@
+package content
+
+import (
+	"testing"
+)
+
+func TestSetGet(t *testing.T) {
+	s := NewSettings()
+	s.Set("hello", "world")
+	s.Set("yes", true)
+	s.Set("no", false)
+	s.Set("cool", -1337)
+
+	if v, ok := s.Get("hello").(string); !ok || v != "world" {
+		t.Error("Should be ok")
+	}
+	if _, ok := s.Get("hello").(int); ok {
+		t.Error("Should not be ok")
+	}
+	if _, ok := s.Get("notset").(int); ok {
+		t.Error("Should not be ok")
+	}
+	if _, ok := s.Get("notset").(string); ok {
+		t.Error("Should not be ok")
+	}
+}
+
+func TestClone(t *testing.T) {
+	s := NewSettings()
+	s.Set("hello", "world")
+	s.Set("yes", true)
+	s2 := s.Clone()
+	s2.Set("no", false)
+	for k := range s.data {
+		if a, b := s.Get(k), s2.Get(k); a != b {
+			t.Errorf("%s != %s", a, b)
+		}
+	}
+	if a, b := len(s.data), len(s2.data); a == b {
+		t.Errorf("Expected different data lengths: %d == %d", a, b)
+	}
+}
+
+func TestMerge(t *testing.T) {
+	s1 := &Settings{map[string]interface{}{
+		"hello": "world",
+		"yes":   true,
+		"no":    3,
+	}}
+	s2 := &Settings{map[string]interface{}{
+		"no":  false,
+		"yes": false,
+		"new": 14,
+	}}
+	exp := map[string]interface{}{
+		"hello": "world",
+		"yes":   false,
+		"no":    false,
+		"new":   14,
+	}
+	s1.Merge(s2)
+	if a, b := len(s1.data), len(exp); a != b {
+		t.Errorf("%d != %d", a, b)
+	}
+	for k, v := range exp {
+		if a, b := v, s1.Get(k); a != b {
+			t.Errorf("%s != %s", a, b)
+		}
+	}
+
+}
