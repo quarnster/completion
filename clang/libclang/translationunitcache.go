2b515b5af27466cfdec1c0b53747a3c1eddf4741
Fredrik Ehnbom <fredrik@ehnbom.nu>
2013-07-03 19:08:07 +0200

    libclang CompleteAt wip

--- clang/libclang/translationunitcache.go
+++ clang/libclang/translationunitcache.go
@@ -3,8 +3,11 @@
 package libclang
 
 import (
+	"bytes"
 	"code.google.com/p/log4go"
-	clang "github.com/sbinet/go-clang"
+	"fmt"
+	"github.com/quarnster/completion/content"
+	"github.com/sbinet/go-clang"
 	"reflect"
 	"sync"
 )
@@ -88,6 +91,7 @@ func (t *TranslationUnitCache) addEx(w workunit) {
 	}
 }
 
+// TODO(q): respect unsaved_files
 func (t *TranslationUnitCache) GetTranslationUnit(filename string, options []string, options_script string, unsaved_files map[string]string) *LockedTranslationUnit {
 	t.Lock()
 	if tu, ok := t.lut[filename]; !ok {
@@ -116,6 +120,69 @@ func (t *TranslationUnitCache) GetTranslationUnit(filename string, options []str
 			t.addEx(workunit{filename, options, options_script, nil})
 		}
 		return tu
+	}
+	return nil
+}
+
+func (t *TranslationUnitCache) CompleteAt(args *content.CompleteAtArgs, res *content.CompletionResult) error {
+	if len(args.Location.File.Contents) != 0 {
+		return fmt.Errorf("Unfortunately completion of unsaved files is currently not supported")
+	}
+	// TODO: Use GetTranslationUnit
+	if tu, ok := t.lut[args.Location.File.Name]; ok {
+		cres := tu.CompleteAt(args.Location.File.Name, int(args.Location.Line), int(args.Location.Column), 0)
+		if !cres.IsValid() {
+			return fmt.Errorf("CompleteResults is not valid")
+		}
+		defer cres.Dispose()
+		for _, r := range cres.Results() {
+			var (
+				buf bytes.Buffer
+			)
+			switch r.CursorKind {
+			case clang.CK_StructDecl, clang.CK_TypedefDecl:
+				for _, c := range r.CompletionString.Chunks() {
+					buf.WriteString(c.Text())
+				}
+				var tt content.Type
+				tt.Flags = content.FLAG_TYPE_CLASS
+				tt.Name.Absolute = buf.String()
+				res.Types = append(res.Types, tt)
+			case clang.CK_FunctionDecl:
+				var (
+					m            content.Method
+					paramstarted bool
+					argCount     int
+				)
+				for _, c := range r.CompletionString.Chunks() {
+					switch k := c.Kind(); k {
+					case clang.CompletionChunk_ResultType:
+						var v content.Variable
+						v.Name.Relative = c.Text()
+						m.Returns = append(m.Returns, v)
+					case clang.CompletionChunk_Placeholder:
+						var v content.Variable
+						v.Type.Name.Relative = c.Text()
+						v.Name.Relative = fmt.Sprintf("arg%d", argCount)
+						argCount++
+						m.Parameters = append(m.Parameters, v)
+					case clang.CompletionChunk_LeftParen:
+						paramstarted = true
+					case clang.CompletionChunk_RightParen, clang.CompletionChunk_Comma:
+					case clang.CompletionChunk_TypedText:
+						if !paramstarted {
+							buf.WriteString(c.Text())
+						}
+					default:
+						log4go.Warn("Unimplemented CompletionChunkKind: %s", k)
+					}
+				}
+				m.Name.Relative = buf.String()
+				res.Methods = append(res.Methods, m)
+			default:
+				log4go.Warn("Unimplemented CursorKind: %s", r.CursorKind)
+			}
+		}
 	}
 	return nil
 }
--- clang/libclang/translationunitcache_test.go
+++ clang/libclang/translationunitcache_test.go
@@ -3,6 +3,7 @@
 package libclang
 
 import (
+	"github.com/quarnster/completion/content"
 	"testing"
 )
 
@@ -10,5 +11,20 @@ func TestTranslationUnitCache(t *testing.T) {
 	cache := NewTranslationUnitCache()
 	if tu := cache.GetTranslationUnit("../testdata/hello.cpp", []string{"-x", "c++"}, "", nil); tu == nil {
 		t.Error("Didn't get a translation unit")
+	}
+	var (
+		args = content.CompleteAtArgs{
+			Location: content.SourceLocation{
+				File:   content.File{Name: "../testdata/hello.cpp"},
+				Line:   4,
+				Column: 1,
+			},
+		}
+		res content.CompletionResult
+	)
+	if err := cache.CompleteAt(&args, &res); err != nil {
+		t.Errorf("Error completing: %v", err)
+	} else {
+		t.Logf("%+v", res)
 	}
 }
