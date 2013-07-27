7d3ed9be10f19910fdc10eb0da4c803d255b0b23
Fredrik Ehnbom <fredrik@ehnbom.nu>
2013-07-27 11:32:54 +0200

    util/scopes: Include the brackets and quotes in the extraction. This makes it easier to parse the output as given by the new Substr method.

--- util/scopes/scopes_manual.go
+++ util/scopes/scopes_manual.go
@@ -1,6 +1,7 @@
 package scopes
 
 import (
+	"bytes"
 	"github.com/quarnster/completion/content"
 	"github.com/quarnster/parser"
 )
@@ -9,20 +10,36 @@ import (
 // Note that it only uses '{' and '}' to deduce scopes, and further
 // processing is likely needed depending on language specifics.
 func Visibility(loc content.SourceLocation) (ret parser.RangeSet) {
-	ret.Add(parser.Range{0, len(loc.File.Contents)})
 	var s SCOPES
 	s.Parse(loc.File.Contents)
 	var rec func(node *parser.Node)
 	pos := int(loc.Offset())
 	rec = func(node *parser.Node) {
 		for _, child := range node.Children {
-			if !child.Range.Inside(pos) {
-				ret = ret.Cut(child.Range)
+			if !child.Range.Inside(pos) || child.Name == "TextScope" || child.Name == "CommentScope" {
+				r := child.Range
+				if child.Name == "BracketScope" || child.Name == "TextScope" {
+					// We want the brackets (or "'s) as part of the result to make the resulting
+					// source code parseable.
+					r.Start += 1
+					r.End -= 1
+				}
+				ret = ret.Cut(r)
 			} else {
 				rec(child)
 			}
 		}
 	}
-	rec(s.RootNode())
+	root := s.RootNode()
+	ret.Add(root.Range)
+	rec(root)
 	return
+}
+
+func Substr(data string, visible parser.RangeSet) string {
+	b := bytes.NewBuffer(nil)
+	for _, r := range visible {
+		b.WriteString(data[r.Start:r.End])
+	}
+	return b.String()
 }
